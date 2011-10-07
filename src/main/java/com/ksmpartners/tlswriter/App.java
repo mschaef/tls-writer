6d68325727712440d4cf9e1e449d0a81bbb49ab0
Mike Schaeffer <schaeffer.michael.a@gmail.com>
2011-10-07 13:57:48 -0400

    Add a thread.

--- src/main/java/com/ksmpartners/tlswriter/App.java
+++ src/main/java/com/ksmpartners/tlswriter/App.java
@@ -1,5 +1,7 @@
 package com.ksmpartners.tlswriter;
 
+import java.io.PrintStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,8 +9,29 @@ public class App
 {
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
+    public static Thread startWriter(final int id,
+                                     final PrintStream out)
+    {
+        Thread th = new Thread() {
+                public void run() {
+                    for(int ii = 0; ii < 1000; ii++)
+                        out.println(id);
+                };
+            };
+
+        th.start();
+
+        return th;
+    }
+
     public static void main( String[] args )
     {
+        try {
+            startWriter(1, System.out).join();
+        } catch(Throwable th) {
+            log.error("Uncaught error.", th);
+        }
+
         log.info("end run.");
     }
 }
