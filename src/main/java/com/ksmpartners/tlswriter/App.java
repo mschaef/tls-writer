31b389667129e85bd63d8d1c16730c2baf0d67de
Mike Schaeffer <schaeffer.michael.a@gmail.com>
2011-10-07 13:49:15 -0400

    Initial commit.

---
+++ src/main/java/com/ksmpartners/tlswriter/App.java
@@ -0,0 +1,14 @@
+package com.ksmpartners.tlswriter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App
+{
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        log.info("end run.");
+    }
+}
