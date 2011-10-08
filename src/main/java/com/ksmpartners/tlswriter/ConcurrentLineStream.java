7363867ff4540e6434c4667c91f591a993006142
Mike Schaeffer <schaeffer.michael.a@gmail.com>
2011-10-07 22:35:30 -0400

    Improve code documentatoin.

--- src/main/java/com/ksmpartners/tlswriter/ConcurrentLineStream.java
+++ src/main/java/com/ksmpartners/tlswriter/ConcurrentLineStream.java
@@ -6,13 +6,30 @@ import java.util.LinkedList;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * ConcurrentLineStream is a wrapper around OutputStream that
+ * automatically adds line based synchronization of writes to the
+ * underlying output stream. PUt another way, multiple threads can
+ * write to a ConcurrentLineStream, and the concurrent stream ensures
+ * that each line of text in the final output will only contain text
+ * from a single thread.
+ */
 public class ConcurrentLineStream
     extends OutputStream
 {
+    /** The underlying output stream. All output ultimately gets
+     * written to this stream. */
+    protected OutputStream underlying = null;
+
+    public ConcurrentLineStream(OutputStream underlying)
+    {
+        this.underlying = underlying;
+    }
+
     private List<StringBuffer> threadLineBuffers =
         new LinkedList<StringBuffer>();
 
-    ThreadLocal<StringBuffer> buf = new ThreadLocal<StringBuffer>() {
+    ThreadLocal<StringBuffer> threadBuf = new ThreadLocal<StringBuffer>() {
         protected StringBuffer initialValue() {
             StringBuffer buf = new StringBuffer();
 
@@ -23,13 +40,6 @@ public class ConcurrentLineStream
             return buf;
         }
     };
-
-    protected OutputStream underlying = null;
-
-    public ConcurrentLineStream(OutputStream underlying)
-    {
-        this.underlying = underlying;
-    }
 
     protected void flushBuffer(StringBuffer buf)
         throws IOException
@@ -42,15 +52,15 @@ public class ConcurrentLineStream
     public void flush()
         throws IOException
     {
-        flushBuffer(buf.get());
+        flushBuffer(threadBuf.get());
 
-        buf.get().setLength(0);
+        threadBuf.get().setLength(0);
     }
 
     public void write(int b)
         throws IOException
     {
-        buf.get().append((char)b);
+        threadBuf.get().append((char)b);
 
         if (b == (int)'\n')
             flush();
